ad948e35561e773c54ec728ba9363daa2b86d4f9
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2026-01-31 08:30:17 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//*****************************************************************************
+//** 44. Find Smallest Letter Greater Than Target                   leetcode **
+//*****************************************************************************
+//** Through sorted ranks the letters stand, in quiet ordered line,          **
+//** We hunt the one that beats our mark, just barely, just in time.         **
+//** If none step forth beyond the goal, we circle back to start,            **
+//** A faster path, a clever loop, where logic plays its part.               **
+//*****************************************************************************
+
+char nextGreatestLetter(char* letters, int lettersSize, char target) {
+    int left = 0;
+    int right = lettersSize - 1;
+    char retval = letters[0];
+
+    while(left <= right)
+    {
+        int mid = left + ((right - left) >> 1);
+
+        if(letters[mid] > target)
+        {
+            retval = letters[mid];
+            right = mid - 1;
+        }
+        else
+        {
+            left = mid + 1;
+        }
+    }
+
+    return retval;
+}
